5f1b5c20cfa4080107983486e7a5dca23ef6dffe
viethung9at1 <hung.nguyen@saritasa.com>
2023-07-31 13:55:48 +0700

    Initial commit

---
+++ Tool_CS/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+
+var key = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\DWMFRAMEINTERVAL", true);
+if (key == null)
+{
+    key = Registry.LocalMachine.CreateSubKey(@"SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations");
+    key.SetValue("DWMFRAMEINTERVAL", 15);
+    return;
+}
+key.Close();
+Registry.LocalMachine.DeleteSubKey(@"SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\DWMFRAMEINTERVAL");
+Process.Start("shutdown.exe", "-r -t 0");
